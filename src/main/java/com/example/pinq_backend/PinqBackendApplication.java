c2ac4b30e8afe02067fa9631e9db5f61a7a111d8
Admin <admin@Adminui-MacBookPro.local>
2026-05-12 11:46:08 +0900

    initial commit

---
+++ src/main/java/com/example/pinq_backend/PinqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pinq_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinqBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pinq_backend/PinqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pinq_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
